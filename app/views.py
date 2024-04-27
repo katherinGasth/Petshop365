90eae9ffd70441888055ede453b15228efd3ea50
afasanigrez-duocuc <ar.fasani@duocuc.cl>
2024-04-27 14:43:57 -0400

    Creación de página para recuperar contraseña

--- app/views.py
+++ app/views.py
@@ -42,3 +42,38 @@ def del_carrito(request):
         carrito.productos.remove(carrito.productos.get(idProducto=idProducto))
         carrito.save()
     return redirect(to="/carrito")
+
+def password_reset_request(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        user = User.objects.filter(email=email).first()
+        if user:
+            # Generar un token único para el usuario y redirigirlo a la página de restablecimiento de contraseña
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            return redirect('password_reset_confirm', uidb64=uid, token=token)
+        else:
+            messages.error(request, 'No se encontró ninguna cuenta asociada a este correo electrónico.')
+    return render(request, 'password_reset_request.html')
+
+def password_reset_confirm(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    # Verificar que el token sea válido
+    if user is not None and default_token_generator.check_token(user, token):
+        if request.method == "POST":
+            password = request.POST['password']
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Tu contraseña ha sido restablecida con éxito.')
+            return redirect('login')
+        return render(request, 'password_reset_confirm.html')
+    else:
+        messages.error(request, 'El enlace de restablecimiento de contraseña es inválido o ha caducado.')
+        return redirect('login')
+
+
